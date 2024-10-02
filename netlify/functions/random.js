be00d2445014c7b6a110368a72495f80a6166f68
Pedro Aragão <pedro.aragao.leite@gmail.com>
2024-10-02 18:39:30 +0200

    change

--- netlify/functions/random.js
+++ netlify/functions/random.js
@@ -3,12 +3,20 @@ import fetch from 'node-fetch';
 export async function handler(event, context) {
     try {
         const response = await fetch('https://bored-api.appbrewery.com/random');
+        console.log('llamada a la api');
+        // Verifica si la respuesta fue exitosa
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
         const data = await response.json();
+
         return {
             statusCode: 200,
             body: JSON.stringify(data),
         };
     } catch (error) {
+        console.error('Error in fetching random activity:', error); // Log para ver en Netlify
         return {
             statusCode: 500,
             body: JSON.stringify({ error: 'Error fetching random activity' }),
